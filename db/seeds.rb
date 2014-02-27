3cb6af299d09711f151943bc3f5988e1ebb3c6d8
Benoit Gantaume <benoit.gantaume@agilidee.com>
2014-02-27 14:32:12 +0100

    Added jo's specs

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@ Activity.create(label: 'Formation')
 PaymentTerm.create(label: '30 jours fin de mois')
 PaymentTerm.create(label: 'A réception de facture')
 
+# VINSON FRÈRES
+# RN7 - NORD
+# 26250 LIVRON SUR DROME
+
+# AZUR PLONGÉE
+# Port de la Madrague
+# 83270 SAINT CYR SUR MER
+
+# CREPS LANGUEDOC-ROUSSILLON
+# Établissement de Montpellier
+# 2 avenue Charles FLAHAULT
+# 34090 MONTPELLIER
