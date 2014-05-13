75c1ae16cdb5c6da30bb9f56958156ee68b79217
Victor Grousset <victor@agilidee.com>
2014-05-13 11:30:38 +0200

    Fix ActiveAdmin PaymentTerm.label permission (#56)
    Label attribute was not added in ActiveAdmi which caused failures when trying to create or update

--- app/admin/payment_term.rb
+++ app/admin/payment_term.rb
@@ -1,6 +1,4 @@
 ActiveAdmin.register PaymentTerm do
-
-  
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
@@ -13,5 +11,6 @@ ActiveAdmin.register PaymentTerm do
   #  permitted << :other if resource.something?
   #  permitted
   # end
-  
+
+  permit_params :label
 end
