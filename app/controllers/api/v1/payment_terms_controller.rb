8f18611baf08f23038256df6d44a8637ba3a23aa
Victor Grousset <victor@agilidee.com>
2014-06-03 15:21:33 +0200

    Add api filter for PaymentTerm (same entity_id)#56
    Return only the payment terms of the same entity as the current_user

--- app/controllers/api/v1/payment_terms_controller.rb
+++ app/controllers/api/v1/payment_terms_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class PaymentTermsController <  ApiController
       def index
-        respond_with PaymentTerm.all
+        respond_with PaymentTerm.where(entity_id: current_user.entity_id)
       end
     end
   end
--- spec/controllers/api/v1/payment_terms_controller_spec.rb
+++ spec/controllers/api/v1/payment_terms_controller_spec.rb
@@ -4,13 +4,27 @@ module Api
   module V1
     describe PaymentTermsController do
       before(:each) do
-        sign_in FactoryGirl.create :user
+        sign_in @user = FactoryGirl.create(:user)
       end
 
       describe '#index' do
         it 'should grant access' do
           get :index, format: :json
           response.status.should eq(200)
+        end
+        it 'should only show payment terms from the same entity as the user' do
+          # create 3 terms(with same entity, other entity, nil)
+          other_entity = FactoryGirl.create :entity
+          FactoryGirl.create :payment_term, entity: @user.entity
+          FactoryGirl.create :payment_term, entity: other_entity
+          FactoryGirl.create :payment_term, entity: nil
+
+          get :index, format: :json
+          payment_terms = JSON.parse(response.body, symbolize_names: true)
+          # collect all the entity_id with no duplicates
+          entity_id_list = (payment_terms.collect { |term| term[:entity_id] }).uniq
+          # it should remain only one id and it should be the one of the user
+          entity_id_list.should == [@user.entity_id]
         end
       end
     end
