bc6f03dc0e9246f71f5bf17e74799464ceaabc68
Victor <victor@tuxayo.net>
2014-07-07 10:19:10 +0200

    Refactor: indentation in Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,5 +2,5 @@ class Customer < ActiveRecord::Base
   belongs_to :entity, inverse_of: :customers
   validates_presence_of :entity
 
-   default_scope{order('name')}
+  default_scope{order('name')}
 end
