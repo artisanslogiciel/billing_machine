e660b9f660a18e752aca48b42d2b3a6fc0266401
marc-agilidee <marc@agilidee.com>
2014-07-07 10:09:11 +0200

    sort customers alphabeticaly by defautlt #138

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -6,4 +6,13 @@ describe Customer do
   end
   it { should belong_to :entity}
   it { should validate_presence_of :entity }
+
+  it 'should display name order by name' do
+    FactoryGirl.create(:customer, name: 'CCLIENT')
+    FactoryGirl.create(:customer, name: 'BCLIENT')
+    FactoryGirl.create(:customer, name: 'DCLIENT')
+    FactoryGirl.create(:customer, name: 'ACLIENT')
+    all_customer_name = Customer.all.collect(&:name)
+    all_customer_name.should == ['ACLIENT','BCLIENT','CCLIENT','DCLIENT']
+  end
 end
