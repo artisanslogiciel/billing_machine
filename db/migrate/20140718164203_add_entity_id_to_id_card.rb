5d7cc76d373dbc0c3feeeb8e70046e32346af8e8
Victor Grousset <victor@agilidee.com>
2014-07-21 15:12:19 +0200

    Finish data migration to add IdCard #123

--- db/migrate/20140718164203_add_entity_id_to_id_card.rb
+++ db/migrate/20140718164203_add_entity_id_to_id_card.rb
@@ -6,10 +6,12 @@ class AddEntityIdToIdCard < ActiveRecord::Migration
   def data
     if ActiveRecord::Base.connection.column_exists?(:invoices, :entity_id)
       Entity.all.each do |entity|
-        IdCard.create(entity_id: entity.id)
+        IdCard.create(entity_id: entity.id, name: entity.name)
       end
       Invoice.all.each do |invoice|
-        invoice.id_card_id = IdCard.where(entity_id: invoice.entity_id)
+        id_card = IdCard.where(entity_id: invoice.entity_id).take
+        invoice.id_card_id = id_card.id
+        invoice.save
       end
     end
   end
