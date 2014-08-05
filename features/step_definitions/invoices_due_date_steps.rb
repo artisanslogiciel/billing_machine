0ed8e6f0169a97cd016d9169b093b3a9ab2c5e3d
tuxayo <victor@tuxayo.net>
2014-08-05 11:36:37 +0200

    Add integration tests for storing due date #165

---
+++ features/step_definitions/invoices_due_date_steps.rb
@@ -0,0 +1,9 @@
+#encoding: utf-8
+
+When(/^he sets a due date$/) do
+  fill_in 'invoice-due-date', with: @due_date = '2014-08-05'
+end
+
+Then(/^the due date is still there$/) do
+  page.should have_field('invoice-due-date', with: @due_date)
+end
