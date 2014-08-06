d263a4efe26e7ff84de43963521a32f1c4cf5b41
tuxayo <victor@tuxayo.net>
2014-08-06 22:22:33 +0200

    Add colors to late invoices #165

--- features/step_definitions/invoices_due_date_steps.rb
+++ features/step_definitions/invoices_due_date_steps.rb
@@ -8,8 +8,16 @@ Then(/^the due date is still there$/) do
   page.should have_field('invoice-due-date', with: @due_date)
 end
 
-Then(/^the invoice paid status should be green$/) do
-  find("#paid")[:class].should include("green")
+Given(/^an existing unpaid invoice with a due date not yet passed$/) do
+  @invoice = FactoryGirl.create(:invoice, id_card: @user.entity.current_id_card,
+                                paid: false,
+                                due_date: (Date.today + 1))
+end
+
+Given(/^an existing unpaid invoice with a due date the same day$/) do
+  @invoice = FactoryGirl.create(:invoice, id_card: @user.entity.current_id_card,
+                                paid: false,
+                                due_date: (Date.today))
 end
 
 Given(/^an existing unpaid invoice with a due date yesterday$/) do
@@ -18,7 +26,24 @@ Given(/^an existing unpaid invoice with a due date yesterday$/) do
                                 due_date: (Date.today - 1))
 end
 
+Given(/^an existing unpaid invoice with a due date (\d+) days ago$/) do |days|
+  @invoice = FactoryGirl.create(:invoice, id_card: @user.entity.current_id_card,
+                                paid: false,
+                                due_date: (Date.today - days.to_i))
+end
+
+Then(/^the invoice paid status should not have a color$/) do
+  find("#paid")[:class].should_not include("green", "orange", "red")
+end
+
+Then(/^the invoice paid status should be green$/) do
+  find("#paid")[:class].should include("green")
+end
+
 Then(/^the invoice paid status should be orange$/) do
-  ap @invoice
   find("#paid")[:class].should include("orange")
+end
+
+Then(/^the invoice paid status should be red$/) do
+  find("#paid")[:class].should include("red")
 end
