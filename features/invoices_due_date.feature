e72e6937ad0d9642609e7253898bab8107f91d12
tuxayo <victor@tuxayo.net>
2014-08-06 22:45:36 +0200

    Refactor invoice tests #165

--- features/invoices_due_date.feature
+++ features/invoices_due_date.feature
@@ -22,34 +22,39 @@ Feature: Invoice due date Management
   Scenario: Paid invoice green in list
     Given an existing paid invoice
     When the user goes to the invoices page
-    Then the invoice paid status should be green
+    Then the invoice paid status should be "green"
 
   @javascript
   Scenario: Unpaid invoice with due date not passed have no color in list
-    Given an existing unpaid invoice with a due date not yet passed
+    Given an existing unpaid invoice
+    And it's due date is not yet passed
     When the user goes to the invoices page
     Then the invoice paid status should not have a color
 
   @javascript
   Scenario: Unpaid invoice with due date the same day have no color in list
-    Given an existing unpaid invoice with a due date the same day
+    Given an existing unpaid invoice
+    And it's due date is the same day
     When the user goes to the invoices page
     Then the invoice paid status should not have a color
 
   @javascript
   Scenario: Unpaid invoice with due date just passed appears orange in list
-    Given an existing unpaid invoice with a due date yesterday
+    Given an existing unpaid invoice
+    And it's due date is yesterday
     When the user goes to the invoices page
-    Then the invoice paid status should be orange
+    Then the invoice paid status should be "orange"
 
   @javascript
   Scenario: Unpaid invoice with due date passed by 15 days appears orange in list
-    Given an existing unpaid invoice with a due date 15 days ago
+    Given an existing unpaid invoice
+    And it's due date is 15 days ago
     When the user goes to the invoices page
-    Then the invoice paid status should be orange
+    Then the invoice paid status should be "orange"
 
   @javascript
   Scenario: Unpaid invoice with due date passed by 16 days appears red in list
-    Given an existing unpaid invoice with a due date 16 days ago
+    Given an existing unpaid invoice
+    And it's due date is 16 days ago
     When the user goes to the invoices page
-    Then the invoice paid status should be red
+    Then the invoice paid status should be "red"
--- features/step_definitions/invoices_due_date_steps.rb
+++ features/step_definitions/invoices_due_date_steps.rb
@@ -8,42 +8,31 @@ Then(/^the due date is still there$/) do
   page.should have_field('invoice-due-date', with: @due_date)
 end
 
-Given(/^an existing unpaid invoice with a due date not yet passed$/) do
+Given(/^an existing unpaid invoice$/) do
   @invoice = FactoryGirl.create(:invoice, id_card: @user.entity.current_id_card,
-                                paid: false,
-                                due_date: (Date.today + 1))
+                                paid: false)
 end
 
-Given(/^an existing unpaid invoice with a due date the same day$/) do
-  @invoice = FactoryGirl.create(:invoice, id_card: @user.entity.current_id_card,
-                                paid: false,
-                                due_date: (Date.today))
+Given(/^it's due date is not yet passed$/) do
+  @invoice.update(due_date: (Date.today + 1))
 end
 
-Given(/^an existing unpaid invoice with a due date yesterday$/) do
-  @invoice = FactoryGirl.create(:invoice, id_card: @user.entity.current_id_card,
-                                paid: false,
-                                due_date: (Date.today - 1))
+Given(/^it's due date is the same day$/) do
+  @invoice.update(due_date: (Date.today))
 end
 
-Given(/^an existing unpaid invoice with a due date (\d+) days ago$/) do |days|
-  @invoice = FactoryGirl.create(:invoice, id_card: @user.entity.current_id_card,
-                                paid: false,
-                                due_date: (Date.today - days.to_i))
+Given(/^it's due date is yesterday$$/) do
+  @invoice.update(due_date: (Date.today - 1))
+end
+
+Given(/^it's due date is (\d+) days ago$/) do |days|
+  @invoice.update(due_date: (Date.today - days.to_i))
 end
 
 Then(/^the invoice paid status should not have a color$/) do
   find("#paid")[:class].should_not include("green", "orange", "red")
 end
 
-Then(/^the invoice paid status should be green$/) do
-  find("#paid")[:class].should include("green")
-end
-
-Then(/^the invoice paid status should be orange$/) do
-  find("#paid")[:class].should include("orange")
-end
-
-Then(/^the invoice paid status should be red$/) do
-  find("#paid")[:class].should include("red")
+Then(/^the invoice paid status should be "(.*?)"$/) do |color|
+  find("#paid")[:class].should include(color)
 end
