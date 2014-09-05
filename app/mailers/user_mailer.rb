121a7fcd94610b791df70fbccb238ed209f5b181
Benoit Gantaume <benoit@agilidee.com>
2014-09-05 10:22:57 +0200

    Tuning default from

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "support@agilidee.com"
+  default from: "postmaster@dorsale.cc"
   def invoice_alert(invoice)
     @invoice = invoice
     users = invoice.entity.users.where(notify_invoices_late_payments: true).map(&:email)
